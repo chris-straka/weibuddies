4c8340f085f0a6644dee0704c1b25c5250f4cec0
Chris Straka <skylake112@outlook.com>
2022-03-09 20:05:55 -0700

    wire up login form part 1/2

--- app/client/src/pages/Home/LoginForm/LoginForm.vue
+++ app/client/src/pages/Home/LoginForm/LoginForm.vue
@@ -1,20 +1,61 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import type { FormRules, FormInst, FormValidationError } from "naive-ui";
 import { NCard, NDivider, NForm, NFormItem, NInput, NButton } from "naive-ui";
+
+const formRef = ref<FormInst | null>(null);
+const formValue = ref({
+  user: {
+    email: "",
+    password: "",
+  },
+});
+
+const rules: FormRules = {
+  email: {
+    required: true,
+    message: "Email is required",
+  },
+  password: {
+    required: true,
+    message: "Password is required",
+  },
+};
+
+const submit = () => {
+  formRef.value?.validate((errors: Array<FormValidationError> | undefined) => {
+    if (!errors) {
+      console.log("yes");
+    } else {
+      console.log(errors);
+    }
+  });
+};
 </script>
 
 <template>
   <n-card>
-    <n-form label-placement="left" label-width="auto">
+    <n-form
+      ref="formRef"
+      label-placement="left"
+      label-width="auto"
+      :model="formValue"
+      :rules="rules"
+    >
       <n-form-item label="Email">
-        <n-input placeholder="Email"></n-input>
+        <n-input placeholder="Email" v-model:value="formValue.user.email" />
       </n-form-item>
       <n-form-item label="Password">
-        <n-input type="password" placeholder="Password"></n-input>
+        <n-input
+          type="password"
+          placeholder="Password"
+          v-model:value="formValue.user.password"
+        />
       </n-form-item>
       <n-divider />
       <div class="buttonContainer">
-        <n-button>Login</n-button>
-        <n-button>Create new account</n-button>
+        <n-button @click.prevent="submit">Login</n-button>
+        <n-button @click.prevent>Create new account</n-button>
       </div>
     </n-form>
   </n-card>
---
+++ app/client/src/store/index.ts
@@ -0,0 +1,5 @@
+import { defineStore } from "pinia";
+
+export const authStore = defineStore("auth", {
+  // TODO
+});
